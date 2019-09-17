dfbf260b96abe12b4fce628c92112f0bd7912e74
taoxuefeng <tao642133424@163.com>
2019-09-17 10:21:17 +0800

    HBase

---
+++ src/main/java/cn/qf/hbase/Demo3_DML.java
@@ -0,0 +1,21 @@
+package cn.qf.hbase;
+
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Table;
+import org.junit.Test;
+
+/**
+ * @ program: hadoopStudy
+ * @ author:  TaoXueFeng
+ * @ create: 2019-09-17 09:41
+ * @ desc:
+ **/
+
+public class Demo3_DML {
+    @Test
+    public void put() {
+        //获取table对象
+
+
+    }
+}
--- src/main/java/cn/qf/hbase/HBaseUtils.java
+++ src/main/java/cn/qf/hbase/HBaseUtils.java
@@ -2,9 +2,12 @@ package cn.qf.hbase;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Table;
+import sun.tools.jconsole.Tab;
 
 import java.io.IOException;
 
@@ -19,13 +22,25 @@ public class HBaseUtils {
     private static final  String ZK_NAME="hbase.zookeeper.quorum";
     private static final  String ZK_URL="10.211.55.28:2181,10.211.55.29:2181,10.211.55.30:2181";
 
-    public static Admin getAdmin() {
+    private static Configuration configuration;
+    private static Connection connection;
+
+    private static final String DEFAULT_TABLE_NAME = "ns1:t1";
+
+    static {
         //1. 获取到配置文件对象
-        Configuration configuration = HBaseConfiguration.create();
+        configuration = HBaseConfiguration.create();
         configuration.set(ZK_NAME, ZK_URL);
         //2. 获取到连接对象
         try {
-            Connection connection = ConnectionFactory.createConnection(configuration);
+            connection = ConnectionFactory.createConnection(configuration);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static Admin getAdmin() {
+
+        try {
             //3. 获取到admin对象
             Admin admin = connection.getAdmin();
             return admin;
@@ -34,6 +49,32 @@ public class HBaseUtils {
             return null;
         }
     }
+
+    public static Table getTable(String tableName) {
+        try {
+            TableName table_Name = TableName.valueOf(tableName);
+            if (getAdmin().tableExists(table_Name)) {
+                Table table = connection.getTable(TableName.valueOf(tableName));
+                return table;
+            }else {
+                return null;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public static Table getTable() {
+        Table table = getTable(DEFAULT_TABLE_NAME);
+        return table;
+    }
+
+    public static void checkConnection() throws IOException {
+        if (connection.isClosed()) {
+            Connection connection = ConnectionFactory.createConnection(configuration);
+        }
+    }
+
     public static void close(Admin admin) {
         if (admin != null) {
             Connection connection = admin.getConnection();
@@ -42,6 +83,15 @@ public class HBaseUtils {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+    public static void close(Table table) {
+        try {
+            if (table != null) {
+                table.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
